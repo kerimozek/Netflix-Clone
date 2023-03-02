d4cac1252228ece9b56fa880ec3b9552176a35c9
Kerim <mehmetkerimozek@gmail.com>
2023-03-02 13:23:16 +0300

    setup general settings

---
+++ Netflix-Clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DownloadsViewController.swift
+//  Netflix-Clone
+//
+//  Created by PSI_ErsanV on 2.03.2023.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+   
+
+}
---
+++ Netflix-Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController.swift
+//  Netflix-Clone
+//
+//  Created by PSI_ErsanV on 2.03.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+    }
+    
+
+ 
+
+}
