a1771016e75819f32cc14e15ebb7834a1f2c7211
Kerim <mehmetkerimozek@gmail.com>
2023-05-15 14:58:48 +0300

    added APICaller

---
+++ Netflix-Clone/Managers/APICaller.swift
@@ -0,0 +1,40 @@
+//
+//  APICaller.swift
+//  Netflix-Clone
+//
+//  Created by PSI_ErsanV on 15.05.2023.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let API_KEY = "fd24fe5ba58021d3f54a2a7c04297951"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+
+
+class APICaller {
+    
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (String) -> Void) {
+        
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                print(results)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+    
+}
